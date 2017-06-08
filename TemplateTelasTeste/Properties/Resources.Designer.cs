d1e51d43121c188372e2ee38f3e4054d605543c5
Flavio Nascimento Jr <flavionascimento2007@hotmail.com>
2017-06-08 05:21:49 -0300

    Outras Refatorações.
    Nome da solução e projeto renomeados

--- TemplateTelasTeste/Properties/Resources.Designer.cs
+++ TemplateTelasTeste/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TemplateTelasTeste.Properties {
+namespace NavKids.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace TemplateTelasTeste.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TemplateTelasTeste.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("NavKids.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
